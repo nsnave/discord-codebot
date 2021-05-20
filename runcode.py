de1374262997e098ef495964a792c955a3977a5e
nsnave <aevans1500@gmail.com>
2021-05-20 02:42:55 -0400

    finished sml

--- runcode.py
+++ runcode.py
@@ -9,10 +9,18 @@ import subprocess
 # Handles running the code in the specified language
 # Returns the output from running the code (stdout and stderr) with the exit status
 class CodeDriver:
-    def handleSub(args):
+    def unpack(result):
+        exit_status = result.returncode
+        output = result.stdout.decode("utf-8") 
+        error = result.stderr.decode("utf-8")   
+    
+        return exit_status, output, error
+    
+    def handleSub(args, input_arg=None):
         return subprocess.run(args, 
                             stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+                            stderr=subprocess.PIPE,
+                            input=input_arg)
 
     def handleCompiled(comp_args, exec_args, exec_path):
         # Compiles the code
@@ -46,21 +54,35 @@ class CodeDriver:
             
             # Runs the code
             if (lang == 'python'):
-                # Runs python script
+                # Runs Python code
                 result = CodeDriver.handleSub(['python3', path])
-
-                exit_status = result.returncode
-                output = result.stdout.decode("utf-8") 
-                error = result.stderr.decode("utf-8")   
+                exit_status, output, error = CodeDriver.unpack(result)
             elif (lang == 'c'):
+                # Runs C code
                 exec_path = path + "-exec"
                 comp_args = ['gcc', '-x', 'c', path, '-o', exec_path]
                 exit_status, output, error = CodeDriver.handleCompiled(comp_args, exec_path, exec_path)
             elif (lang == 'java'):
+                # Runs Java code
                 exec_path = path + "-exec.java"
                 comp_args = ['cp', path, exec_path]
                 exec_args = ['java', exec_path]
                 exit_status, output, error = CodeDriver.handleCompiled(comp_args, exec_args, exec_path)
+            elif (lang == "sml"):
+                # Runs SML/NJ Code
+                input_arg = 'use "' + path + '";\n'
+                result = CodeDriver.handleSub(['sml'], input_arg.encode('utf-8'))
+                exit_status, output, error = CodeDriver.unpack(result)
+                
+                if (exit_status == 0):
+                    # Removes unneeded interpreter output
+                    output_lines = output.split('\n')
+                    adj = -3 if (output_lines[-3] == "val it = () : unit") else -2
+                    output = '\n'.join(output_lines[2:adj])
+
+                    # Changes exit status if needed
+                    if (adj == -2):
+                        exit_status = 1 
             else:
                 exit_status = -1
                 output = ""
